887bc37d9845e3ff9480678276760f071cf993b6
Bruno Anhaia <hiroki.zx@gmail.com>
2020-09-26 17:28:11 +0000

    Changed repo structure

--- main.cpp
+++ main.cpp
@@ -1,39 +1,7 @@
 #include <iostream>
 #include <vector>
-#include "lib.h"
-
-int partition (std::vector<int>* value, int first_index, int last_index) {
-  int pivot_index = first_index;
-
-  /* Setting pivotCounter member  */
-  int lessThanPivotCount = pivot_index;
-
-  for (int i = pivot_index + 1; i < value->size(); i++){
-    /* We just found a number smaller or equal our pivot_index,
-       we will add a counter and then swap those numbers 
-     */
-    if (value->at(i) < value->at(pivot_index)) {
-      lessThanPivotCount++;
-      std::swap(value->at(lessThanPivotCount), value->at(i));
-    }
-  }
-  /* Swaping pivot member with last found number  */
-  std::swap(value->at(pivot_index), value->at(lessThanPivotCount));
-
-  return lessThanPivotCount;
-}
-
-void quick_sort(std::vector<int>* value, int left_index, int right_index){
-  if (left_index < right_index) {
-
-    int pivot_index = partition(value, left_index, right_index);
-    
-    /* Evaluate left side of pivot */
-    quick_sort(value, left_index, pivot_index - 1);
-    /* Evaluate right side of pivot */
-    quick_sort(value, pivot_index + 1, right_index);
-  }
-}
+#include "QuickSort/lib.cpp"
+#include "QuickSort/quick_sort.cpp"
 
 int main() {
 
